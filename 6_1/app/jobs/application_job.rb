7ee668ee0736b1bbc6fdd42aaa85c9957cd5d084
Hideki Miyamoto <hideki0145@gmail.com>
2022-12-10 01:27:10 +0900

    Add: Sidekiqを追加する
    非同期処理の為にSidekiqを追加する。

--- 6_1/app/jobs/application_job.rb
+++ 6_1/app/jobs/application_job.rb
@@ -4,4 +4,6 @@ class ApplicationJob < ActiveJob::Base
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
+
+  rescue_from(ActiveJob::DeserializationError) { |e| LogUtility.error_log(e) }
 end
--- 6_1/config/routes.rb
+++ 6_1/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  require 'sidekiq/web'
+  mount Sidekiq::Web, at: '/sidekiq'
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
