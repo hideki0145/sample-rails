0b2e8db866703726f49d45db1dce50681844ecc9
Hideki Miyamoto <hideki0145@gmail.com>
2022-12-09 23:57:17 +0900

    Improve: pumaを改善する
    production環境用の設定を改善する。

--- 6_1/config/puma.rb
+++ 6_1/config/puma.rb
@@ -15,7 +15,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -41,3 +41,10 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+if Rails.env.production?
+  bind "unix://#{Rails.root.join('tmp/puma.sock')}"
+  stdout_redirect Rails.root.join('log/stdout.log'), Rails.root.join('log/stderr.log')
+else
+  port ENV.fetch('PORT', 3000)
+end
