bdc3041076a79580ecfa733322a2810c592594d4
Hideki Miyamoto <hideki0145@gmail.com>
2022-12-10 00:49:31 +0900

    Add: CarrierWaveを追加する
    添付ファイルを扱う為にCarrierWaveを追加する。
    実行したコマンドは以下の通り。
    
    ```sh
    bin/rails generate uploader File
    ```

--- 6_1/app/mailers/application_mailer.rb
+++ 6_1/app/mailers/application_mailer.rb
@@ -1,4 +1,15 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  private
+
+  def attach_files(files)
+    files.each do |file|
+      attachments[file.original_filename] = {
+        content: File.read(file.file.current_path, mode: 'rb'),
+        transfer_encoding: :binary
+      }
+    end
+  end
 end
