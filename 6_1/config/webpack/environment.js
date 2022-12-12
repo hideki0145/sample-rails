d89571cac7e93ed62d4cb51f155e30844c8ce6e5
Hideki Miyamoto <hideki0145@gmail.com>
2022-12-12 23:07:51 +0900

    Add: Vueを追加する
    WebpackerにVueを追加する。
    実行したコマンドは以下の通り。
    
    ```sh
    bin/rails webpacker:install:vue
    ```

--- 6_1/config/webpack/environment.js
+++ 6_1/config/webpack/environment.js
@@ -6,8 +6,16 @@ config.performance = {
   maxAssetSize: 10485760,
   maxEntrypointSize: 20971520,
 };
+config.resolve.alias = {
+  vue$: "vue/dist/vue.esm.js",
+};
 
 const typescript = require("./loaders/typescript");
 environment.loaders.prepend("typescript", typescript);
+
+const { VueLoaderPlugin } = require("vue-loader");
+const vue = require("./loaders/vue");
+environment.plugins.prepend("VueLoaderPlugin", new VueLoaderPlugin());
+environment.loaders.prepend("vue", vue);
 
 module.exports = environment
