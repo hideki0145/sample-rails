afc0b7af2e624ab4054800ba410b9ede55e1e9ed
Hideki Miyamoto <hideki0145@gmail.com>
2022-12-12 17:53:57 +0900

    Improve: Webpackerを改善する
    パフォーマンスヒントのサイズ閾値を変更する。

--- 6_1/config/webpack/environment.js
+++ 6_1/config/webpack/environment.js
@@ -1,5 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
 environment.splitChunks();
+const config = environment.toWebpackConfig();
+config.performance = {
+  maxAssetSize: 10485760,
+  maxEntrypointSize: 20971520,
+};
 
 module.exports = environment
