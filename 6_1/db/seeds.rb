19405203cf9a9258bc04aab3eedb20514630bc97
Hideki Miyamoto <hideki0145@gmail.com>
2022-12-13 20:14:35 +0900

    Improve: Seedデータの追加方法を改善する
    Seedデータの追加方法を改善する。

--- 6_1/db/seeds.rb
+++ 6_1/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env.development?
+  require 'factory_bot'
+  include FactoryBot::Syntax::Methods
+  FactoryBot.definition_file_paths = [Rails.root.join('spec/factories')]
+  FactoryBot.reload
+end
+
+table_names = %w[
+]
+table_names.each do |table_name|
+  path = Rails.root.join("db/seeds/#{Rails.env}/#{table_name}.rb")
+  if File.exist?(path)
+    puts "Creating #{table_name}..."
+    require path
+  end
+end
