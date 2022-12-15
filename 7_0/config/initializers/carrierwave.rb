1258047018ce762f3e9c2f4314dc9e7cf6dd44a2
Hideki Miyamoto <hideki0145@gmail.com>
2022-12-15 21:56:24 +0900

    Add: CarrierWaveを追加する
    添付ファイルを扱う為にCarrierWaveを追加する。
    実行したコマンドは以下の通り。
    
    ```sh
    bin/rails generate uploader File
    ```

---
+++ 7_0/config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:print:]]/
+
+CarrierWave.configure do |config|
+  # These permissions will make dir and files available only to the user running
+  # the servers
+  config.permissions = 0o0600
+  config.directory_permissions = 0o0700
+  config.storage = :file
+  # This avoids uploaded files from saving to public/ and so
+  # they will not be available for public (non-authenticated) downloading
+  config.root = Rails.root
+end
