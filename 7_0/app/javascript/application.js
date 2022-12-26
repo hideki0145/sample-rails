77342f8bf983495f9f126696192a5c66288d59e5
Hideki Miyamoto <hideki0145@gmail.com>
2022-12-27 00:35:00 +0900

    Add: Mrujsを追加する
    rails-ujsとの互換性を保つ為にMrujsを追加する。

--- 7_0/app/javascript/application.js
+++ 7_0/app/javascript/application.js
@@ -1,3 +1,10 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import mrujs from "mrujs";
+import { MrujsTurbo } from "mrujs/plugins";
+
+Turbo.session.drive = false;
+mrujs.start({
+  plugins: [MrujsTurbo()],
+});
